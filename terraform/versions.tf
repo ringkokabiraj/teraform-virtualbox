41ca37f05c8024ca643aea69fec76cd983324288
ringkokabiraj <rinku.bcc@gmail.com>
2025-04-26 00:49:15 +0600

    Trigger: apply terraform to start Ubuntu VM

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">= 1.0.0"
-
   required_providers {
     virtualbox = {
-      source  = "terra-farm/virtualbox"
-      version = ">= 0.2.2"
+      source  = "local/virtualbox"
+      version = "0.2.2"
     }
   }
 }
