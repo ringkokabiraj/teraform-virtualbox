a2b8f845f5817c5f612a1c474877675724b2d6f8
ringkokabiraj <rinku.bcc@gmail.com>
2025-04-25 20:28:55 +0600

    Trigger workflow again

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,35 @@
 provider "virtualbox" {
-  # No config needed if VirtualBox is installed
+  version = ">= 0.2.2"
 }
 
 resource "virtualbox_vm" "ubuntu_vm" {
-  name      = "UbuntuVM"
-  image     = "https://cloud-images.ubuntu.com/minimal/releases/focal/release/ubuntu-20.04-minimal-cloudimg-amd64.vdi"
-  cpus      = 2
-  memory    = 1024
+  name   = "ubuntu-22-04-vm"
+  cpus   = 2
+  memory = "2048 mib"
+
+  image = "E:\Software\ubuntu-22.04.5-live-server-amd64.iso"
 
   network_adapter {
-    type           = "hostonly"
-    host_interface = "VirtualBox Host-Only Ethernet Adapter"
+    type = "nat"
   }
 
   storage_controller {
     name = "SATA Controller"
-  }
+    type = "sata"
 
-  disk {
-    image = "ubuntu-20.04-minimal.vdi"
+    device {
+      port     = 0
+      device   = 0
+      type     = "hdd"
+      image    = ""E:\Software\ubuntu-22-04.vdi"
+      size     = 15000 # size in MB
+    }
+
+    device {
+      port     = 1
+      device   = 0
+      type     = "dvd"
+      image    = "E:\Software\ubuntu-22.04.5-live-server-amd64.iso"
+    }
   }
 }
