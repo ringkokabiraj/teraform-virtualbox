e390dd6b6c1a6234a41aaff8998605730b131720
ringkokabiraj <rinku.bcc@gmail.com>
2025-04-25 21:12:30 +0600

    Trigger: apply terraform to start Ubuntu VM

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "virtualbox_vm" "ubuntu_vm" {
   cpus   = 2
   memory = "2048 mib"
 
-  image = "E:\Software\ubuntu-22.04.5-live-server-amd64.iso"
+  image = "E:/Software/ubuntu-22.04.5-live-server-amd64.iso"
 
   network_adapter {
     type = "nat"
@@ -21,7 +21,7 @@ resource "virtualbox_vm" "ubuntu_vm" {
       port     = 0
       device   = 0
       type     = "hdd"
-      image    = ""E:\\Software\\ubuntu-22-04.vdi"
+      image    = ""E:/Software/ubuntu-22-04.vdi"
       size     = 15000 # size in MB
     }
 
@@ -29,7 +29,7 @@ resource "virtualbox_vm" "ubuntu_vm" {
       port     = 1
       device   = 0
       type     = "dvd"
-      image    = "E:\Software\ubuntu-22.04.5-live-server-amd64.iso"
+      image    = "E:/Software/ubuntu-22.04.5-live-server-amd64.iso"
     }
   }
 }
